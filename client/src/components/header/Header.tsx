b29b47d9ddb069bcd4a0e43e17c0b60dec0fb158
TheGrids <zolotar333@gmail.com>
2023-02-14 18:25:56 +0300

    add navbar && test darkmode

---
+++ client/src/components/header/Header.tsx
@@ -0,0 +1,47 @@
+import { FC } from 'react';
+import { useTheme } from '../../hooks/useTheme'
+import searchSVG from '../../assets/search.svg'
+import arrowSVG from '../../assets/arrow.svg'
+import './Header.css'
+
+const Header: FC = () => {
+
+    const [theme, handleChange] = useTheme('dark');
+
+    return (
+        <div>
+            <header>
+                <nav>
+                    <div className='nav_box'>
+                        <div className='cname_box'>
+                            <a className='cname_box_text'>GreenAreal</a>
+                            <span className='cname_box_catalog'>
+                                <div style={{pointerEvents: 'none', userSelect: 'none', margin: '0 12px'}}>Каталог</div>
+                                <img src={arrowSVG} alt="" style={{pointerEvents: 'none', userSelect: 'none'}}/>
+                            </span>
+                        </div>
+                        <div className='search_box'>
+                            <input type="search" placeholder='Поиск по сайту'/>
+                            <span className='search_button'>
+                                <img src={searchSVG} alt="" style={{pointerEvents: 'none', userSelect: 'none'}}/>
+                            </span>
+                        </div>
+                        <div className='nav_buttons'>
+                            <div className='nav_some_button'></div>
+                            <div className='nav_some_button'></div>
+                            <div className='nav_some_button'></div>
+                            <div className='nav_some_button'></div>
+                        </div>
+                    </div>
+                </nav>
+            </header>
+            <span>Изменить тему</span>
+            <label className="switch">
+                <input type="checkbox" onChange={handleChange} checked={theme === 'dark'} />
+                <span className="slider"></span>
+            </label>
+        </div>
+    );
+};
+
+export default Header;
